988a1f142f0c92866c626685d47ad2480a1d6eaa
usomarriba <usomarriba@kx.com>
2023-07-18 01:30:54 -0400

    Add levels (some).

--- Assets/Scripts/EndOfLevelHandler.cs
+++ Assets/Scripts/EndOfLevelHandler.cs
@@ -26,6 +26,6 @@ public class EndOfLevelHandler : MonoBehaviour
 
     void LoadNextLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class Water : MonoBehaviour
 {
     [SerializeField] float riseSpeed = 0.5f;
+    [SerializeField] float initDelay = 0f;
+    float timePassed;
     Vector3 scaleChange;
 
     // Start is called before the first frame update
     void Start()
     {
-        scaleChange = new Vector3(0, riseSpeed * Time.deltaTime, 0);
+        scaleChange = new Vector3(0, riseSpeed, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.localScale += scaleChange;
+        timePassed += Time.deltaTime;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (timePassed > initDelay)
+        {
+            transform.localScale += scaleChange * Time.deltaTime;
+        }
     }
 }
